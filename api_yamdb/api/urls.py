23ed4150845180b99425b653c514d9a1ed612d7c
Artem Karpov <ikarp97@gmail.com>
2022-10-07 12:19:32 +0300

    added views and urls for genres categories and titles

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (CommentViewSet, ReviewViewSet, UserViewSet, get_token,
-                    register)
+from .views import (CommentViewSet, ReviewViewSet, UserViewSet, GenreViewSet,
+                    CategoryViewSet, TitleViewSet, get_token, register)
 
 router_v1 = DefaultRouter()
 
@@ -15,9 +15,23 @@ router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet, basename='comments',
 )
+router_v1.register(
+    'categories',
+    CategoryViewSet,
+    basename='сategories'
+)
+router_v1.register(
+    'titles',
+    TitleViewSet,
+    basename='titles'
+)
+router_v1.register(
+    'genres',
+    GenreViewSet,
+    basename='genres'
+)
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
     path('v1/', include(router_v1.urls)),
     path('v1/auth/signup/', register, name='register'),
     path('v1/auth/token/', get_token, name='token'),
