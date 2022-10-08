a25bb5f191b0e97b657e69b4f8e2a872c5e8d496
Artem Karpov <ikarp97@gmail.com>
2022-10-08 11:02:26 +0300

    review pep8 fixes

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,7 +5,7 @@ class IsAdminOrReadonly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated and (
-                     request.user.is_admin or request.user.is_superuser
+                    request.user.is_admin or request.user.is_superuser
                 )))
 
 
